3753191a78569ca69bd51db0a629b6c7b01fb0db
Haloio29 <aureliolucio5@gmail.com>
2024-02-10 19:27:42 +0700

    update materi week-1

---
+++ week-1/materi-math.js
@@ -0,0 +1,13 @@
+// math yang sering di gunakan
+
+// Keatas itu ceil
+let data = Math.ceil(20.8788);
+console.log(data);
+
+// Standar itu round
+let angka = Math.round(9.2992);
+console.log(angka);
+
+// Kebawah itu floor
+let hasil = Math.floor(10.5898);
+console.log(hasil);

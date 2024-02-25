b2bddbc96916f784f1df76be22e12eddc32bc77a
Haloio29 <aureliolucio5@gmail.com>
2024-02-25 13:35:37 +0700

    add soal quiz week-2

---
+++ week-2/quiz/soal-3.js
@@ -0,0 +1,19 @@
+/*
+diberikan sebuah function groupAnimals(animals) yang menerima satu parameter berupa array,
+fungsi ini akan me-return array 2 dimensi
+*/
+function groupAnimals(animals) {
+  let hasil = [];
+  for (let i = 0; i <= animals.length; i++) {
+    
+  }
+  hasil.push();
+}
+
+// TEST CASES
+console.log(groupAnimals(["cacing", "ayam", "kuda", "anoa", "kancil"]));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(
+  groupAnimals(["cacing", "ayam", "kuda", "anoa", "kancil", "unta", "cicak"])
+);
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]

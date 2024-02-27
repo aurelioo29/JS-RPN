f1250c9eafed2cece359fa445cb7128560edbd8b
Haloio29 <aureliolucio5@gmail.com>
2024-02-27 14:40:19 +0700

    unfinish soal quiz week-2

--- week-2/quiz/soal-2.js
+++ week-2/quiz/soal-2.js
@@ -8,11 +8,21 @@ kelompok ke-3 (baris ke-3) merupakan angka-angka kelipatan 3
 Contoh jika arr inputan adalah [45, 20, 21, 2, 7] maka output: [ [ 20, 2 ], [ 7 ], [ 45, 21 ] ]
 */
 function mengelompokkanAngka(arr) {
-  // you can only write your code here!
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 3 === 0) {
+      result.push([arr[2][i]]);
+    } else if (arr[i] % 2 === 0) {
+      result.push([arr[0][i]]);
+    } else if (arr[i] % 2 === 1) {
+      result.push([arr[1][i]]);
+    }
+  }
+  return result;
 }
 
 // TEST CASES
 console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
-console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
-console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
-console.log(mengelompokkanAngka([])); // [ [], [], [] ]
+// console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+// console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+// console.log(mengelompokkanAngka([])); // [ [], [], [] ]

0b82f3897eff8105e7b06e7240e1afd536202767
Haloio29 <aureliolucio5@gmail.com>
2024-02-28 20:32:23 +0700

    finish soal-3 quiz week-2

--- week-2/quiz/soal-3.js
+++ week-2/quiz/soal-3.js
@@ -3,17 +3,26 @@ diberikan sebuah function groupAnimals(animals) yang menerima satu parameter ber
 fungsi ini akan me-return array 2 dimensi
 */
 function groupAnimals(animals) {
+  let sorting = animals.sort();
   let hasil = [];
-  for (let i = 0; i <= animals.length; i++) {
-    
+  let penampung = [];
+  for (let i = 0; i < sorting.length; i++) {
+    if (i === 0) {
+      penampung.push(sorting[i]);
+    } else if (penampung[0][0] === sorting[i][0]) {
+      penampung.push(sorting[i]);
+    } else {
+      hasil.push(penampung);
+      penampung = [];
+      penampung.push(sorting[i]);
+    }
   }
-  hasil.push();
+  hasil.push(penampung);
+  return hasil;
 }
 
 // TEST CASES
-console.log(groupAnimals(["cacing", "ayam", "kuda", "anoa", "kancil"]));
-// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(["cacing", "ayam", "kuda", "anoa", "kancil"])); // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 console.log(
   groupAnimals(["cacing", "ayam", "kuda", "anoa", "kancil", "unta", "cicak"])
-);
-// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+); // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]

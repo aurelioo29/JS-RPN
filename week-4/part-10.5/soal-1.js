b1422c9fbfb7ce051413117e6a9b860f7b21df45
aurelioo29 <aureliolucio5@gmail.com>
2024-05-03 08:32:11 +0700

    add Soal-1 (part-10.5)

---
+++ week-4/part-10.5/soal-1.js
@@ -0,0 +1,45 @@
+/*
+Implementasikan function sorting dan getTotal untuk mendapatkan angka yang paling besar dan mengetahui berapa kali angka tersebut muncul di dalam arrNumber.
+
+Dengan HANYA mengubah code di dalam 2 function yang diberikan (sorting dan getTotal). Dilarang mengubah isi dalam function mostFrequentLargestNumbers!
+*/
+function sorting(arrNumber) {
+  return arrNumber.sort((a, b) => b - a);
+}
+
+function getTotal(arrNumber) {
+  let largest = arrNumber[0];
+  let time = 0;
+
+  if (arrNumber.length === 0) return "''";
+
+  for (let i = 0; i < arrNumber.length; i++) {
+    if (largest === arrNumber[i]) {
+      time++;
+    } else {
+      break;
+    }
+
+    return `angka paling besar adalah ${largest} dan jumlah kemunculan sebanyak ${time} kali`;
+  }
+}
+
+function mostFrequentLargestNumbers(arrNumber) {
+  var listSort = sorting(arrNumber);
+  var countHighest = getTotal(listSort);
+  return countHighest;
+}
+
+console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+
+console.log(
+  mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150])
+);
+//'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+
+console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+
+console.log(mostFrequentLargestNumbers([]));
+//''
